9e4c672a2992c9f5d473ae3eba6cbe753ceacc32
Gabriel Rodrigues <gabrielsilva.8850@outlook.com>
2025-06-05 08:59:26 -0300

    adicionando arquivos estáticos

--- galeria/views.py
+++ galeria/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render  # noqa: F401
-from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('<h1>Alura Space</h1><p>Seja bem-vindo ao espaço<p>')
+    return render(request, 'galeria/index.html')
--- setup/settings.py
+++ setup/settings.py
@@ -61,7 +61,7 @@ ROOT_URLCONF = 'setup.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -127,6 +127,13 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_DIR_FILES = [
+    os.path.join(BASE_DIR, 'setup/static')
+
+]
+
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
