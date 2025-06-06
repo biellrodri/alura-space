5166d0b4c7eb8a779216e5e6e97032b9ae842521
Gabriel Rodrigues <gabrielsilva.8850@outlook.com>
2025-06-06 08:28:35 -0300

    inserindo imagens e nova página

--- galeria/views.py
+++ galeria/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render  # noqa: F401
 
 def index(request):
     return render(request, 'galeria/index.html')
+
+
+def imagem(request):
+    return render(request, 'galeria/imagem.html')
